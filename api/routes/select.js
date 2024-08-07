33d2f6d774eaeb4fb22b994e1b704a39a79cff9c
muahand-neyma <losylos084@gamil.com>
2024-08-07 11:20:02 -0700

    ggg

--- api/routes/select.js
+++ api/routes/select.js
@@ -2,10 +2,11 @@
 
 
 import express from 'express'
+const router = express.Router() 
 import {SelectText ,getSelect  } from '../controllers/select.js'
 import {verifyToken} from '../utils/verifyUser.js'
 
-const router = express.Router() 
+
 
 router.post("/sele",SelectText)
 router.delete("/:id",getSelect)
